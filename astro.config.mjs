68cee62158b549d011773e4b6c976e28d4c55bb2
Stiven Castillo <stivencastillo.90@gmail.com>
2024-11-12 15:27:16 -0500

    Add post page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,54 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
-
-import tailwind from '@astrojs/tailwind';
-
-import icon from 'astro-icon';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import remarkToc from "remark-toc";
+import rehypePresetMinify from "rehype-preset-minify";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeToc from "rehype-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://stiven.dev',
-    integrations: [mdx(), sitemap(), tailwind(), icon()],
-    i18n: {
-        locales: ['en', 'es'],
-        defaultLocale: 'es',
-        routing: {
-            prefixDefaultLocale: false
-        }
+  site: "https://stiven.dev",
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      remarkRehype: { footnoteLabel: "Footnotes" },
+      rehypePlugins: [
+        rehypePresetMinify,
+        rehypeHeadingIds,
+        [
+          rehypeToc,
+          {
+            headings: ["h1", "h2", "h3"],
+            cssClasses: {
+              toc: "toc-post",
+              link: "toc-link",
+            },
+          },
+        ],
+        [rehypeAutolinkHeadings, { behavior: 'append' }]
+      ],
+      gfm: false,
+      extendMarkdownConfig: true
+    }),
+    sitemap(),
+    tailwind(),
+    icon(),
+  ],
+  markdown: {
+    syntaxHighlight: "prism",
+    gfm: true,
+    shikiConfig: { theme: "dracula" },
+    remarkPlugins: [remarkToc],
+  },
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: false,
     },
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    category: reference("categories"),
+    description: z.string().optional(),
+    publishedAt: z.date(),
+    updatedAt: z.date(),
+    heroImage: z.string().optional(),
+    published: z.boolean(),
+    tags: z.array(z.string()),
+    lang: z.enum(["en", "es"]),
+  }),
 });
 
-export const collections = { blog };
+const categories = defineCollection({
+  type: "data",
+  schema: z.object({
+    slug: z.string(),
+    name: z.string(),
+  }),
+});
+
+export const collections = { blog, categories };
