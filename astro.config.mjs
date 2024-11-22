ed2bb2c337a974cd83692df6a270821b0f6dbc53
Stiven Castillo <stivencastillo.90@gmail.com>
2024-11-22 11:15:22 -0500

    Add posts and blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import remarkToc from "remark-toc";
 import rehypePresetMinify from "rehype-preset-minify";
-import { rehypeHeadingIds } from "@astrojs/markdown-remark";
-import rehypeToc from "rehype-toc";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
 // https://astro.build/config
 export default defineConfig({
   site: "https://stiven.dev",
@@ -18,18 +14,6 @@ export default defineConfig({
       remarkRehype: { footnoteLabel: "Footnotes" },
       rehypePlugins: [
         rehypePresetMinify,
-        rehypeHeadingIds,
-        [
-          rehypeToc,
-          {
-            headings: ["h1", "h2", "h3"],
-            cssClasses: {
-              toc: "toc-post",
-              link: "toc-link",
-            },
-          },
-        ],
-        [rehypeAutolinkHeadings, { behavior: 'append' }]
       ],
       gfm: false,
       extendMarkdownConfig: true
@@ -42,7 +26,6 @@ export default defineConfig({
     syntaxHighlight: "prism",
     gfm: true,
     shikiConfig: { theme: "dracula" },
-    remarkPlugins: [remarkToc],
   },
   i18n: {
     locales: ["en", "es"],
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,27 @@
+import type { CollectionEntry } from "astro:content";
+export type GroupPostType = {year: number; items: CollectionEntry<"blog">[]}
+
 export function formatDate(date: Date): string {
   return date.toLocaleDateString("en-US", {
-    year: "numeric",
     month: "long",
     day: "numeric",
   });
+}
+
+export function groupPosts(array: CollectionEntry<"blog">[]): Array<GroupPostType> {
+  const groups = array.reduce((result: any, note) => {
+    const {data} = note;
+    const year = new Date(data.updatedAt).getFullYear();
+    const isGroupExisting: any = result.find((group: any) => group.year === year);
+
+    if (isGroupExisting) {
+      isGroupExisting.items.push(note);
+    } else {
+      result.push({ year: year, items: [note] });
+    }
+
+    return result;
+  }, []);
+
+  return groups;
 }
